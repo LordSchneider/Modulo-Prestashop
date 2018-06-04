52715f53929c892e3e59d101865ec5d013f988df
LordSchneider <39808270+LordSchneider@users.noreply.github.com>
2018-06-04 12:11:14 -0600

    Update micoso.php

--- micoso.php
+++ micoso.php
@@ -12,6 +12,15 @@
             $this->bootstrap=true;
             parent::__construct();
         }
+        public function processConfiguration(){
+            if (Tools::isSubmit('mymod_pc_form')){
+                $enable_grades=Tools::getValue('enable_grades');
+                $enable_comments=Tools::('enable_comments');
+                Configuration::updateValue('MYMOD_GRADES',$enable_grades);
+                Condifuration::updateValue('MYMOD_COMMENTS',$enable_comments);
+                $this->context->smarty->assign('confirmation','ok');
+            }
+        }
         public function getContent(){
             return $this->display(__FILE__,'getContent.tpl');
         }
