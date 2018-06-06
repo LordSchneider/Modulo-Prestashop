998beaddeefc55bfcaf16f576a97e70ca9ca2ed0
LordSchneider <39808270+LordSchneider@users.noreply.github.com>
2018-06-06 16:49:58 -0600

    Update micoso.php

--- micoso.php
+++ micoso.php
@@ -40,20 +40,31 @@
             return $this->display(__FILE__,'getContent.tpl');
         }
         public function processProductTabContent(){
-            if (Tools::isSubmit('mymod_pc_submit_comment')) {
+            if (Tools::isSubmit('mymod_pc_submit_comment'))  {
                 $id_product=Tools::getValue('id_product');
                 $grade=Tools::getValue('grade');
-                $comment=Tools::getValue('comment');
+                $comment=pSQL(Tools::getValue('comment'));
                 $insert=array(
                     'id_product'=>(int) $id_product,
                     'grade'=>(int) $grade,
-                    'comment'=>pSQL($comment),
-                    'date_add'=>date('Y-m-d-H:i:s'),
+                    'comment'=>$comment,
+                    'date_add'=>date('Y-m-d H:i:s'),
                 );
                 Db::getInstance()->insert('mymod_comment',$insert);
             }
         }
         public function hookDisplayProductTabContent($params){
+            $this->processProductTabContent();
+            $this->assignProductTabContent();
             return $this->display(__FILE__,'displayProductTabContent.tpl');
+        }
+        public function assignProductTabContent(){
+            $enable_grades=Configuration::get('MYMOD_GRADES');
+            $enable_comments=Configuration::get('MYMOD_COMMENTS');
+            $id_product=Tools::getValue('id_product');
+            $comments=Db::getInstance()->executeS('SELECT * FROM '._DB_PREFIX_.'mymod_comment WHERE id_product = '.(int)$id_product );
+            $this->context->smarty->assign('enable_grades',$enable_grades);
+            $this->context->smarty->assign('enable_comments',$enable_comments);
+            $this->context->smarty->assign('comments',$comments);
         }
     }
