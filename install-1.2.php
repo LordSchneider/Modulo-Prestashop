f702b28cd5cf55dd6adaa55688c40c187596a08a
LordSchneider <39808270+LordSchneider@users.noreply.github.com>
2018-06-08 16:43:51 -0600

    Update install-1.2.php

--- install-1.2.php
+++ install-1.2.php
@@ -1,8 +1,7 @@
 <?php
     function upgrade_module_1_2($module){
-        $sql_file=dirname(__FILE__).'/sql/install-1.2.sql';
-        if (!module->loadSQLFile($sql_file)) {
-            return false;
-        }
+        $sql_file=(__FILE__).'/sql/install-1.2.sql';
+        if(!$module->loadSQLFile($sql_file))
+        return false;
         return true;
     }
