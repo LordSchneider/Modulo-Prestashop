76f9613797a74c0e00a2f23fe2f0310eb718396d
LordSchneider <39808270+LordSchneider@users.noreply.github.com>
2018-06-08 16:42:33 -0600

    Update micoso.php

--- micoso.php
+++ micoso.php
@@ -1,21 +1,50 @@
 <?php
     class MiCoso extends Module
     {
+        
+        public function loadSQLFile($sql_file){
+            $sql_content=file_get_contents($sql_file);
+            $sql_content=str_replace('PREFIX_',_DB_PREFIX_,$sql_content);
+            $sql_requests=preg_split("/;\s*[\r\n]+/",$sql_content);
+            $result=true;
+            foreach($sql_requests as $request)
+            if(!empty($request))
+            $result &=Db::getInstance()->execute(trim($request));
+            return $result;
+        }
         public function __construct()
         {
             $this->name='Micoso';
             $this->tab='front_office_features';
-            $this->displayName='Modulo de pruebas';
-            $this->version='1.1';
+            $this->displayName=$this->l('Modulo de pruebas');
+            $this->version='1.2';
             $this->author='Ignacio Casado';
-            $this->description='Modulo de prueba';
+            $this->description=$this->l('Modulo de prueba.');
+            $this->ps_versions_compliancy=array('min'=>'1.5.2','max'=>'1.6.1.19');
             $this->bootstrap=true;
             parent::__construct();
-
         }
         public function install(){
-            parent::install();
-            $this->registerHook('displayProductTabContent');
+            if (!parent::install()) {
+                return false;
+            }
+            $sql_file=dirname(__FILE__).'/install/install.sql';
+            $this->loadSQLFile($sql_file);
+            return false;
+            if (!$this->registerHook('displayProductTabContent'))
+            return false;
+            Configuration::updateValue('MYMOD_GRADES','1');
+            Configuration::updateValue('MYMOD_COMMENTS','1');
+            return true;
+        }
+        public function uninstall(){
+            if (!parent::uninstall())
+            return false;
+            $sql_file=dirname(__FILE__).'/install/uninstall.sql';
+            if (!$this->loadSQLFile($sql_file))
+            return false;
+            Configuration::deleteByName('MYMOD_GRADES');
+            Configuration::deleteByName('MYMOD_COMMENTS');
             return true;
         }
         public function processConfiguration(){
@@ -40,13 +69,12 @@
             return $this->display(__FILE__,'getContent.tpl');
         }
         public function processProductTabContent(){
+
             if (Tools::isSubmit('mymod_pc_submit_comment'))  {
                 $id_product=Tools::getValue('id_product');
                 $grade=Tools::getValue('grade');
                 if ($grade!=0) {
                     $comment=Tools::getValue('comment');
-                    echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-                    echo $comment;
                     $comment=strval(pSQL($comment));
                     if ($comment=='') {
                         
@@ -58,6 +86,7 @@
                         'date_add'=>date('Y-m-d H:i:s'),
                     );
                     Db::getInstance()->insert('mymod_comment',$insert);
+                    $this->context->smarty->assign('new_comment_posted','true');
                     }
                     
                 }
@@ -74,6 +103,10 @@
             $enable_comments=Configuration::get('MYMOD_COMMENTS');
             $id_product=Tools::getValue('id_product');
             $comments=Db::getInstance()->executeS('SELECT * FROM '._DB_PREFIX_.'mymod_comment WHERE id_product = '.(int)$id_product );
+            $this->context->controller->addCSS($this->_path.'views/css/micoso.css','all');
+            $this->context->controller->addCSS($this->_path.'views/css/star-rating.css','all');
+            $this->context->controller->addJS($this->_path.'views/js/star-rating.js');
+            $this->context->controller->addJS($this->_path.'views/js/micoso.js');
             $this->context->smarty->assign('enable_grades',$enable_grades);
             $this->context->smarty->assign('enable_comments',$enable_comments);
             $this->context->smarty->assign('comments',$comments);
