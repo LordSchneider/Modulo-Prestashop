65be82735dc033238f0c9736cb2833e9f8affb75
LordSchneider <39808270+LordSchneider@users.noreply.github.com>
2018-06-11 16:46:47 -0600

    Update install.sql

--- install.sql
+++ install.sql
@@ -1,3 +1,3 @@
-CREATE TABLE IF NOT EXISTS `PREFIX_mymod_comment` (`id_mymod_comment` int(11) NOT NULL AUTO_INCREMENT, 
-`id_product` int(11) NOT NULL, `grade` tinyint(1) NOT NULL, 
-`comment` text NOT NULL,`date_add` datetime NOT NULL,PRIMARY KEY(`id_mymod_comment`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+CREATE TABLE IF NOT EXISTS ps_mymod_comment (id_mymod_comment int(11) NOT NULL AUTO_INCREMENT, 
+id_product int(11) NOT NULL,namen VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, grade tinyint(1) NOT NULL, 
+comment text NOT NULL,date_add datetime NOT NULL,PRIMARY KEY(id_mymod_comment)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
