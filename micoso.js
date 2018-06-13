6067a4425b0da43be66083d1f70294b15c2782ee
LordSchneider <39808270+LordSchneider@users.noreply.github.com>
2018-06-13 10:48:46 -0600

    Delete micoso.js

--- micoso.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	if ($('#mymodcomments-content-tab').attr('data-scroll') == 'true')
-		$.scrollTo('#mymodcomments-content-tab', 1200);
-});
-
-$(document).ready(function () {
-	$('.rating').rating();
-});
