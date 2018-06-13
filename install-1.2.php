5e599df46acb539af2147e05aba854d988ea6603
LordSchneider <39808270+LordSchneider@users.noreply.github.com>
2018-06-13 10:43:04 -0600

    Delete install-1.2.php

--- install-1.2.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function upgrade_module_1_2($module){
-        $sql_file=(__FILE__).'/sql/install-1.2.sql';
-        if(!$module->loadSQLFile($sql_file))
-        return false;
-        return true;
-    }
