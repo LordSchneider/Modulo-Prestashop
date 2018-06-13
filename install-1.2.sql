069480953c8503cdedc051202cda5841f26ebd33
LordSchneider <39808270+LordSchneider@users.noreply.github.com>
2018-06-13 10:46:40 -0600

    Delete install-1.2.sql

--- install-1.2.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `PREFIX_mymod_comment` ADD `nombre` VARCHAR(255) NOT NULL
-AFTER `id_product`, ADD `apellido` VARCHAR(255) NOT NULL AFTER `nombre`,
-ADD `email` VARCHAR(255) NOT NULL AFTER `apellido`
